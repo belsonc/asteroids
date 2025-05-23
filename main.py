0d28b2005f20996bfe49052f2a910fc12c3f2e70
belsonc <craig.belson@gmail.com>
2025-05-22 21:15:02 -0400

    B: added .gitignore file, added code to check if window closed

--- main.py
+++ main.py
@@ -20,6 +20,9 @@ def main():
     screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
 
     while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
         pygame.display.flip()
 
