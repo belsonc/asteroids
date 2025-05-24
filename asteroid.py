fc847f77c95454a8eb068d413c347de4201ed60e
belsonc <craig.belson@gmail.com>
2025-05-23 21:51:39 -0400

    F: added code to create and move asteroids, create asteroid field

---
+++ asteroid.py
@@ -0,0 +1,16 @@
+#creation of asteroid class
+
+from constants import *
+import pygame
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", (int(self.position.x), int(self.position.y)), self.radius, 2)
+
+    #moving the asteroid
+    def update(self, dt):
+        self.position += (self.velocity * dt)
--- main.py
+++ main.py
@@ -2,10 +2,10 @@
 # the open-source pygame library
 # throughout this file
 import pygame
-
 import constants
-
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     pygame.init()
@@ -15,8 +15,18 @@ def main():
     x = constants.SCREEN_WIDTH / 2
     y = constants.SCREEN_HEIGHT / 2
 
+    #creating containers/groups
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable,)
+
     player = Player(x, y)
-    
+    asteroidfield = AsteroidField()
+
     print("Starting Asteroids!")
     print(f"Screen width: {constants.SCREEN_WIDTH}")
     print(f"Screen height: {constants.SCREEN_HEIGHT}")
@@ -27,8 +37,9 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        updatable.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
